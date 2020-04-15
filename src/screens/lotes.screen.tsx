8662ab18d480c3710c5c4601e83e06c72e5005c4
Diego Alvarado <diegoalvarado26@hotmail.com>
2020-04-15 18:47:09 -0300

    lotes list

--- src/screens/lotes.screen.tsx
+++ src/screens/lotes.screen.tsx
@@ -1,7 +1,10 @@
 import {createStackNavigator} from '@react-navigation/stack';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {SafeAreaView, Text} from 'react-native';
 import DrawerButton from '../components/drawer.button';
+import {ListItem} from 'react-native-elements';
+import {FlatList} from 'react-native-gesture-handler';
+const axios = require('axios').default;
 
 const Stack = createStackNavigator();
 
@@ -17,10 +20,54 @@ function LotesNavigator({navigation}) {
   );
 }
 
-function LotesScreen() {
+async function GetLotes() {
+  return await axios({
+    method: 'get',
+    url: 'http://192.168.0.101:3500/lotes/propietario/all',
+  }).then((response) => response.data);
+}
+
+const list = [
+  {
+    name: 'Amy Farha',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President',
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman',
+  },
+];
+
+const keyExtractor = (item, index) => item.id;
+
+const renderItem = ({item}) => (
+  <ListItem
+    title={item.street}
+    subtitle={item.nickname}
+    bottomDivider
+    // chevron
+  />
+);
+
+ function LotesScreen() {
+  const [lotes, setLotes] = useState([]);
+
+  useEffect (() => {
+    setLotes(GetLotes())
+  });
+
   return (
     <SafeAreaView>
       <Text>List of lotes goes here</Text>
+      <FlatList
+        keyExtractor={keyExtractor}
+        data={lotes}
+        renderItem={renderItem}
+      />
     </SafeAreaView>
   );
 }
