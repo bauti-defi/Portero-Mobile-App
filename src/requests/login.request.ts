951d6320d20a63070b7f2845a0b8aea9db2f825e
Bautista-Baiocchi-lora <bautistabaiocchilora@gmail.com>
2020-04-25 18:57:55 -0300

    add device type to login request

--- src/requests/login.request.ts
+++ src/requests/login.request.ts
@@ -4,4 +4,10 @@ export const login = async (
   email: string,
   password: string,
   device_id: string,
-) => await axios.post('/auth/login', {email, password, mid: device_id});
+) =>
+  await axios.post('/auth/login', {
+    email,
+    password,
+    mid: device_id,
+    type: 'mobile',
+  });
